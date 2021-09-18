b83e3ea65b4611bb5a643d87ad1eac2b2546ea8c
Patrick Kim <patrick.yrkim@gmail.com>
2021-09-18 00:51:17 -0700

    Add functionality to save data to local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import NotesList from './components/NotesList';
 import Search from './components/Search';
@@ -26,6 +26,25 @@ const App = () => {
   const [searchText, setSearchText] = useState("");
 
   const [darkMode, setDarkMode] = useState(false);
+
+  // save to local storage
+  useEffect(() => {
+    // first parameter is the key
+    // second paramter is the data we want to save (good practice to stringify data before saving to local storage)
+    localStorage.setItem('react-notes-app-data', JSON.stringify(notes))
+  }, [notes]); // dependency array, whenever whatever is in the [] changes, it updates
+
+  // retrieve items from local storage
+  useEffect(() => {
+    // have to parse the data in JS object from the JSON we saved into local storage
+    // will retrieve the data with the same key
+    const savedNotes = JSON.parse(localStorage.getItem('react-notes-app-data'))
+
+    // check if we recevied any notes from local storage, if we did then set into state
+    if (savedNotes) {
+      setNotes(savedNotes)
+    }
+  }, []) // when dependency array is empty, it will only run on the first load and not anymore after that
 
   const addNote = (text) => {
     const date = new Date();
